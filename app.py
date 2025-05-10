45f7898a393acaa8a910bc4c283d313ab3d1adcf
ALMAkojc525 <jani.kojc1@almamater.si>
2025-05-10 18:10:58 +0200

    TEST DEPLOY 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Minecraft Admin UI is working! *** TEST DEPLOY ***"
+    return "Minecraft Admin UI is working! *** TEST DEPLOY 2 ***"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
