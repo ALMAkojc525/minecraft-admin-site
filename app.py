a88b332d09202b573655133f421d6914f1f3cfdf
Chris1193 <christian.adam@almamater.si>
2025-05-10 18:43:31 +0200

    Testni site

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# Simulirana \"baza podatkov\"
+users = []
+
 @app.route('/')
-def home():
-    return "Minecraft Admin UI is working! *** TEST DEPLOY 3 ***"
+def index():
+    return render_template('index.html', users=users)
+
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        username = request.form['username']
+        name = request.form['name']
+        surname = request.form['surname']
+        users.append({'username': username, 'name': name, 'surname': surname})
+        return redirect(url_for('index'))
+    return render_template('add_user.html')
+
+@app.route('/delete_user/<username>')
+def delete_user(username):
+    global users
+    users = [user for user in users if user['username'] != username]
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
